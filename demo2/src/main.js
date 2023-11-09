e5049a4da0e89f903c92807f8c61ba8475e4e16d
Haden Bennett <hadenbennett444@gmail.com>
2023-11-09 13:56:20 -0600

    Fuck it

--- demo2/src/main.js
+++ demo2/src/main.js
@@ -7,11 +7,11 @@ import { BiFacebook } from "oh-vue-icons/icons";
 import { BiLinkedin } from "oh-vue-icons/icons";
 import { HiMail } from "oh-vue-icons/icons";
 import { BiTelephoneFill } from "oh-vue-icons/icons";
-import { HiSolidChevronDown } from "oh-vue-icons/icons"
+import { HiSolidChevronDown,BiXLg,BiJustify } from "oh-vue-icons/icons"
 
 const app = createApp(App)
 
-addIcons(BiFacebook, BiLinkedin,HiMail,BiTelephoneFill,HiSolidChevronDown);
+addIcons(BiFacebook, BiLinkedin,HiMail,BiTelephoneFill,HiSolidChevronDown,BiXLg,BiJustify);
 
 app.use(router)
 app.component("v-icon", OhVueIcon);
